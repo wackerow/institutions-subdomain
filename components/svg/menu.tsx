3766b9621fe58cb6de2f4abec295299db2706c8a
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-09-23 22:27:19 -0700

    chore: import svgs, update using currentColor

--- components/svg/menu.tsx
+++ components/svg/menu.tsx
@@ -3,13 +3,14 @@ const Menu = (props: React.SVGProps<SVGSVGElement>) => (
     width="30"
     height="17"
     viewBox="0 0 30 17"
-    fill="none"
+    stroke="currentColor"
     xmlns="http://www.w3.org/2000/svg"
+    strokeWidth="2"
     {...props}
   >
-    <path d="M30 1.53949H0" stroke="#D7931E" stroke-width="2" />
-    <path d="M25.25 8.53949H0" stroke="#D7931E" stroke-width="2" />
-    <path d="M30 15.5395H0" stroke="#D7931E" stroke-width="2" />
+    <path d="M30 1.53949H0" />
+    <path d="M25.25 8.53949H0" />
+    <path d="M30 15.5395H0" />
   </svg>
 )
 
---
+++ components/svg/twitter.tsx
@@ -0,0 +1,14 @@
+const Twitter = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="17"
+    height="16"
+    viewBox="0 0 17 16"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M13.2611 0.956543H15.7145L10.3545 7.31039L16.6602 15.9565H11.7229L7.8558 10.7127L3.43104 15.9565H0.976092L6.70921 9.16039L0.660156 0.956543H5.72275L9.21824 5.74963L13.2611 0.956543ZM12.4 14.4335H13.7595L4.98409 2.39961H3.52521L12.4 14.4335Z" />
+  </svg>
+)
+
+export default Twitter
