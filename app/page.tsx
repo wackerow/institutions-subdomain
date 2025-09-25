a3f2397cceeb83d1629501f7a5d2901181b3c330
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-09-24 18:36:34 -0700

    feat: add initial hero to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,56 @@
+import Image, { StaticImageData } from "next/image"
+
+import Hero from "@/components/Hero"
+import EthGlyph from "@/components/svg/eth-glyph"
+import { InfiniteSlider } from "@/components/ui/infinite-slider"
+
+import { cn } from "@/lib/utils"
+
+import blackRock from "@/public/logos/black-rock.png"
+import citi from "@/public/logos/citi.png"
+import coinbase from "@/public/logos/coinbase.png"
+import etoro from "@/public/logos/etoro.png"
+import fidelity from "@/public/logos/fidelity.png"
+import jpMorgan from "@/public/logos/jp-morgan.png"
+import mastercard from "@/public/logos/mastercard.png"
+import robinhood from "@/public/logos/robinhood.png"
+import sony from "@/public/logos/sony.png"
+import standardChartered from "@/public/logos/standard-chartered.png"
+import swift from "@/public/logos/swift.png"
+import ubs from "@/public/logos/ubs.png"
+
+const logos: { src: StaticImageData; alt: string; className?: string }[] = [
+  { src: blackRock, alt: "blackRock logo" },
+  { src: citi, alt: "citi logo" },
+  { src: coinbase, alt: "coinbase logo" },
+  { src: etoro, alt: "etoro logo" },
+  { src: fidelity, alt: "fidelity logo" },
+  { src: jpMorgan, alt: "jpMorgan logo" },
+  { src: mastercard, alt: "mastercard logo" },
+  { src: robinhood, alt: "robinhood logo" },
+  { src: sony, alt: "sony logo" },
+  { src: standardChartered, alt: "standardChartered logo" },
+  { src: swift, alt: "swift logo" },
+  { src: ubs, alt: "ubs logo" },
+]
 export default function Home() {
   return (
-    <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-      Main
-    </main>
+    <>
+      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+        <Hero heading="The Institutional Liquidity Layer" shape={EthGlyph}>
+          <InfiniteSlider speedOnHover={32} gap={56} className="py-10">
+            {logos.map(({ src, alt, className }) => (
+              <Image
+                key={alt}
+                src={src}
+                alt={alt}
+                className={cn("h-8 w-auto grayscale", className)}
+              />
+            ))}
+          </InfiniteSlider>
+        </Hero>
+        Main
+      </main>
+    </>
   )
 }
---
+++ components/svg/eth-glyph.tsx
@@ -0,0 +1,15 @@
+const EthGlyph = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="177"
+    height="302"
+    viewBox="0 0 177 302"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M88.6377 301.366V301.368L88.6367 301.367L0.640625 172.332L88.6367 228.59L176.64 172.332L88.6377 301.366Z" />
+    <path d="M88.6367 0.790039L176.64 154.151L88.6377 210.415L0.640625 154.151L88.6367 0.790039Z" />
+  </svg>
+)
+
+export default EthGlyph
