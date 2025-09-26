647dc29eca8a1e536631706ddc44f52207eda184
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-09-25 17:16:27 -0700

    feat: big numbers section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image, { StaticImageData } from "next/image"
 
+import BigNumber from "@/components/BigNumber"
 import Hero from "@/components/Hero"
 import EthGlyph from "@/components/svg/eth-glyph"
 import { InfiniteSlider } from "@/components/ui/infinite-slider"
@@ -33,24 +34,70 @@ const logos: { src: StaticImageData; alt: string; className?: string }[] = [
   { src: swift, alt: "swift logo" },
   { src: ubs, alt: "ubs logo" },
 ]
+
+// TODO: Fetch live data
+const bigNumbers: { value: string; label: string }[] = [
+  {
+    value: "+90%",
+    label: "+ RWA Marketshare: On Ethereum and its L2s",
+  },
+  {
+    value: "10Y",
+    label: "Years Uninterrupted uptime and liveness",
+  },
+  {
+    value: "1.1m",
+    label: "validators securing the network",
+  },
+  {
+    value: "$140B+",
+    label: "RWA Marketshare: On Ethereum and its L2s",
+  },
+  {
+    value: "$87B+",
+    label: "DeFi TVL- 65%+ of all chains",
+  },
+  {
+    value: "$12B+",
+    label: "24 Hour DEX Volume - Ecosystem Average in 2025",
+  },
+]
+
 export default function Home() {
   return (
-    <>
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Hero heading="The Institutional Liquidity Layer" shape={EthGlyph}>
-          <InfiniteSlider speedOnHover={32} gap={56} className="py-10">
-            {logos.map(({ src, alt, className }) => (
-              <Image
-                key={alt}
-                src={src}
-                alt={alt}
-                className={cn("h-8 w-auto grayscale", className)}
-              />
+    <main className="row-start-2 flex flex-col items-center sm:items-start">
+      <Hero heading="The Institutional Liquidity Layer" shape={EthGlyph}>
+        <InfiniteSlider speedOnHover={32} gap={56} className="py-10">
+          {logos.map(({ src, alt, className }) => (
+            <Image
+              key={alt}
+              src={src}
+              alt={alt}
+              className={cn("h-8 w-auto grayscale", className)}
+            />
+          ))}
+        </InfiniteSlider>
+      </Hero>
+      <article className="max-w-8xl mx-auto w-full space-y-20 px-4 py-10 sm:px-10 sm:py-20 md:space-y-40">
+        <section className="flex gap-20 max-lg:flex-col">
+          <div className="flex flex-col gap-y-10 max-lg:items-center">
+            <h2 className="text-h3 max-lg:mx-auto max-lg:text-center lg:max-w-md">
+              Ethereum is the Backbone of the Onchain Economy
+            </h2>
+            <a href="#" className="primary w-fit text-lg">
+              Live Data
+            </a>
+          </div>
+          <div className="grid grid-cols-[auto_auto] gap-14 max-sm:hidden">
+            {bigNumbers.map(({ value, label }, idx) => (
+              <BigNumber key={idx} value={value} className="xl:w-xs">
+                {label}
+              </BigNumber>
             ))}
-          </InfiniteSlider>
-        </Hero>
-        Main
-      </main>
-    </>
+          </div>
+          <div className="sm:hidden">TODO: Mobile slider</div>
+        </section>
+      </article>
+    </main>
   )
 }
---
+++ components/BigNumber/index.tsx
@@ -0,0 +1,22 @@
+import type { ComponentProps } from "react"
+
+import { cn } from "@/lib/utils"
+
+type BigNumberProps = Pick<ComponentProps<"div">, "className" | "children"> & {
+  value: string
+}
+
+const BigNumber = ({ value, className, children }: BigNumberProps) => (
+  <div
+    className={cn("flex flex-col items-center gap-2 text-center", className)}
+  >
+    {value && <p className="text-h3 font-bold tracking-[0.055rem]">{value}</p>}
+    {children && (
+      <p className="text-muted-foreground mx-auto max-w-52 tracking-[0.02rem]">
+        {children}
+      </p>
+    )}
+  </div>
+)
+
+export default BigNumber
