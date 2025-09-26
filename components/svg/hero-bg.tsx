028c16a7be5dea7b8f5c51267243547e99d6ae0d
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-09-25 19:47:48 -0700

    refactor: hero-bg logic

--- components/svg/hero-bg.tsx
+++ components/svg/hero-bg.tsx
@@ -1,44 +1,43 @@
-const STROKE_WIDTH = 2
-const STROKE_GAP = 8
-const STROKE_COUNT = 42
-const BOX_WIDTH = 670
-const MIN_LENGTH = 270
-const MAX_LENGTH = 600
+const HeroBg = ({
+  width = 670, // pixels
+  strokeWidth = 2, // pixels
+  gap = 8, // pixels
+  lineCount = 42, // pixels
+  ...props
+}: Omit<React.SVGProps<SVGSVGElement>, "height"> & {
+  gap?: number
+  lineCount?: number
+}) => {
+  const minLength = 0.4 * +width
+  const maxLength = 0.9 * +width
+  const xRange = +width - minLength
+  const segmentHeight = +strokeWidth + gap
+  const height = segmentHeight * lineCount
 
-const segmentHeight = STROKE_WIDTH + STROKE_GAP
-const boxHeight = segmentHeight * STROKE_COUNT
-const xRange = BOX_WIDTH - MIN_LENGTH
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox={`0 0 ${width} ${height}`}
+      stroke="currentColor"
+      strokeWidth={strokeWidth}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      {Array.from({ length: lineCount }).map((_, idx) => {
+        const x = Math.random() * xRange
+        const y = (idx + 0.5) * segmentHeight
+        const maxLineLength = Math.min(maxLength, +width - x)
+        const length = minLength + Math.random() * (maxLineLength - minLength)
+        const d = `M${x} ${y}H${length + x}`
 
-const opacityCurve = (b: number): number => {
-  const pow = 1 / 2
-  const k = 1 / 1.2
-  const c = 0.15
-  return k * b ** pow + c
+        const opacity = 0.8 * Math.random() + 0.2
+
+        const id = "line-index-" + idx
+
+        return <path key={id} id={id} d={d} strokeOpacity={opacity} />
+      })}
+    </svg>
+  )
 }
-
-const HeroBg = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    width={BOX_WIDTH}
-    height={boxHeight}
-    viewBox={`0 0 ${BOX_WIDTH} ${boxHeight}`}
-    stroke="currentColor"
-    strokeWidth={STROKE_WIDTH}
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    {Array.from({ length: STROKE_COUNT }).map((_, idx) => {
-      const x = Math.random() * xRange
-      const y = (idx + 0.5) * segmentHeight
-      const maxLineLength = Math.min(MAX_LENGTH, BOX_WIDTH - x)
-      const length = MIN_LENGTH + Math.random() * (maxLineLength - MIN_LENGTH)
-      const d = `M${x} ${y}H${length + x}`
-
-      const opacity = opacityCurve(Math.random())
-
-      const id = "line-index-" + idx
-
-      return <path key={id} id={id} d={d} strokeOpacity={opacity} />
-    })}
-  </svg>
-)
 export default HeroBg
