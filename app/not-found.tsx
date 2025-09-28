c2ec26e7aa1732fb79c95811ba2e22c8209aebe9
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-09-28 16:16:20 -0700

    feat: add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+import Hero from "@/components/Hero"
+import EthGlyph from "@/components/svg/eth-glyph"
+
+export default function Page() {
+  return (
+    <main className="row-start-2 flex flex-col items-center sm:items-start">
+      <Hero heading="Oops!" shape={EthGlyph}>
+        Page not found...
+      </Hero>
+      <Link href="/" className="css-forward-arrow mx-auto mb-20 w-fit text-xl">
+        Return home
+      </Link>
+    </main>
+  )
+}
