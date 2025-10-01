6aceba4e29e3d68c9a538a4f0f60c2eca9a44e54
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-10-01 15:17:12 -0700

    wip: hero layout and sizing

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -24,20 +24,26 @@ const Hero = ({
       className
     )}
   >
-    <div className="max-w-8xl mx-auto grid grid-cols-1 px-10 md:grid-cols-2">
-      <div className="place-self-center">
+    <div className="max-w-8xl relative mx-auto grid grid-cols-1 px-10 md:h-[420px] md:grid-cols-2">
+      <div className="z-10 place-self-center">
         <h1 className="max-md:text-center">{heading}</h1>
         {children}
       </div>
-      <div className="relative h-[420px]">
-        {/* // TODO: Debug hero bg sizing */}
-        <HeroBg className="max-w-full place-self-center" />
+      {/* // TODO: Debug hero bg sizing */}
+      <div className="relative inset-y-0 end-0 z-0 place-items-center max-md:grid md:absolute">
+        <div
+          className="bg-background group-has-[.css-primary-invert]/body:bg-primary absolute inset-0 max-md:hidden"
+          style={{
+            mask: "linear-gradient(to right, white 0%, transparent 40%)",
+          }}
+        />
+        <HeroBg className="h-full max-w-full place-self-center" />
         {Shape && (
           <Shape className="group-has-[.css-primary-invert]/body:fill-primary fill-background absolute inset-0 place-self-center" />
         )}
       </div>
     </div>
-    <div className="w-screen py-10">{beneath && beneath}</div>
+    <div className="w-screen py-10">{beneath}</div>
   </div>
 )
 
