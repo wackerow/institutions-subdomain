4d9ae8efcc7b938c1017fe66eb213b001df8823b
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-10-08 12:51:07 -0700

    feat: add ui/library-card

---
+++ components/ui/library-card.tsx
@@ -0,0 +1,109 @@
+import * as React from "react"
+import Image, { type ImageProps } from "next/image"
+import Link, { LinkProps } from "next/link"
+
+import { isValidDate } from "@/lib/utils/date"
+import { cn } from "@/lib/utils/index"
+
+function LibraryCard({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card"
+      className={cn("group space-y-4", className)}
+      {...props}
+    />
+  )
+}
+
+function LibraryCardHeader({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-header"
+      className={cn("h-52 overflow-hidden", className)}
+      {...props}
+    />
+  )
+}
+
+function LibraryCardImage({ className, alt, ...props }: ImageProps) {
+  return (
+    <Image
+      data-slot="card-image"
+      alt={alt || ""}
+      className={cn(
+        "size-full object-cover transition-transform group-has-[a:hover]:scale-105 group-has-[a:hover]:transition-transform",
+        className
+      )}
+      placeholder="blur"
+      sizes="(max-width: 640px) 100vw, (max-width: 1400px) 44vw, 608px"
+      {...props}
+    />
+  )
+}
+
+function LibraryCardTitle({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <h3
+      data-slot="card-title"
+      className={cn("text-h5 tracking-[0.03rem]", className)}
+      {...props}
+    />
+  )
+}
+
+function LibraryCardTitleLink({
+  className,
+  href,
+  ...props
+}: Omit<LinkProps, "children"> &
+  Pick<React.ComponentProps<"a">, "className" | "children">) {
+  return (
+    <Link
+      data-slot="card-title-link"
+      href={href}
+      className={cn("hover:underline", className)}
+      {...props}
+    />
+  )
+}
+
+function LibraryCardDate({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<"div">) {
+  const date = isValidDate(children as string)
+    ? new Intl.DateTimeFormat("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+      }).format(new Date(children as string))
+    : children
+  return (
+    <div
+      data-slot="card-date"
+      className={cn(
+        "text-muted-foreground text-sm font-medium tracking-[0.0175rem]",
+        className
+      )}
+      {...props}
+    >
+      {date}
+    </div>
+  )
+}
+
+export {
+  LibraryCard,
+  LibraryCardDate,
+  LibraryCardHeader,
+  LibraryCardImage,
+  LibraryCardTitle,
+  LibraryCardTitleLink,
+}
