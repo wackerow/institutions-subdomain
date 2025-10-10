94dc8411cbfebddfce4276ae826547923b268276
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-10-10 00:02:05 -0700

    chore: layer-2 soon placeholder

--- app/layer-2/page.tsx
+++ app/layer-2/page.tsx
@@ -5,16 +5,14 @@ import Hero from "@/components/Hero"
 export default function Page() {
   return (
     <main className="row-start-2 flex flex-col items-center sm:items-start">
-      <Hero heading="H1 TITLE" shape="layers-2">
-        Hero subtext
-      </Hero>
+      <Hero heading="Layer 2 for Enterprise" shape="layers-2"></Hero>
       <article className="max-w-8xl mx-auto w-full space-y-10 px-4 py-10 sm:px-10 sm:py-20 md:space-y-20">
         <section
-          id="SECTION-ID"
+          id="coming-soon"
           className="flex gap-10 max-lg:flex-col md:gap-16"
         >
           <h2 className="text-h3s max-lg:mx-auto max-lg:text-center lg:w-lg lg:max-w-lg lg:shrink-0">
-            LEADING H2
+            Soon™
           </h2>
           <div className=""></div>
         </section>
