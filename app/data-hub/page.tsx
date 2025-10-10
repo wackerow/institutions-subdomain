8eebdbb688f6e991cb9e0d66f86f7f8f9b60feee
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-10-10 11:36:51 -0700

    template: data-hub

--- app/data-hub/page.tsx
+++ app/data-hub/page.tsx
@@ -6,6 +6,7 @@ import RealWorldAssets from "@/components/data/rwa"
 import UltrasoundMoney from "@/components/data/ultrasound-money"
 import ValidatorCount from "@/components/data/validator-count"
 import Hero from "@/components/Hero"
+import { Card } from "@/components/ui/card"
 
 import fetchRWAStablecoins from "../api/rwaStablecoins/fetch"
 
@@ -14,26 +15,69 @@ export default async function Page() {
 
   return (
     <main className="row-start-2 flex flex-col items-center sm:items-start">
-      <Hero heading="Data hub" shape="chart-no-axes-combined"></Hero>
+      <Hero
+        heading="Data Hub: Real-Time Intelligence"
+        shape="chart-no-axes-combined"
+      >
+        <p>
+          Ethereum&apos;s onchain data provides an immutable and transparent
+          foundation for institutional analysis and reporting.
+        </p>
+        <p>
+          Track live, real-time data for mainnet activity, Layer-2 scaling,
+          tokenized assets, and DeFi markets.
+        </p>
+      </Hero>
       <article className="max-w-8xl mx-auto w-full space-y-10 px-4 py-10 sm:px-10 sm:py-20 md:space-y-20">
-        <section id="SECTION-ID" className="">
+        <section id="overview" className="space-y-4">
           <h2 className="text-h3-mobile sm:text-h3 max-lg:mx-auto max-lg:text-center lg:w-lg lg:max-w-lg lg:shrink-0">
-            In progress... coming soon™
+            Overview
           </h2>
-          <div className="">
-            <ValidatorCount />
-            <DeFiTotalValueLocked />
-            <Layer2Data />
-            <UltrasoundMoney />
-            <RealWorldAssets chartData={stablecoinChartData} />
-          </div>
+          <Card>Soon™</Card>
         </section>
+
+        <section id="defi" className="space-y-4">
+          <h2 className="text-h3-mobile sm:text-h3 max-lg:mx-auto max-lg:text-center lg:w-lg lg:max-w-lg lg:shrink-0">
+            Decentralized Finance
+          </h2>
+          <Card>Soon™</Card>
+        </section>
+
+        <section id="stablecoins" className="space-y-4">
+          <h2 className="text-h3-mobile sm:text-h3 max-lg:mx-auto max-lg:text-center lg:w-lg lg:max-w-lg lg:shrink-0">
+            Stablecoins
+          </h2>
+          <Card>Soon™</Card>
+        </section>
+
+        <section id="rwa" className="space-y-4">
+          <h2 className="text-h3-mobile sm:text-h3 max-lg:mx-auto max-lg:text-center lg:w-lg lg:max-w-lg lg:shrink-0">
+            Real-World Assets
+          </h2>
+          <Card>Soon™</Card>
+        </section>
+
+        <section id="layer-2" className="space-y-4">
+          <h2 className="text-h3-mobile sm:text-h3 max-lg:mx-auto max-lg:text-center lg:w-lg lg:max-w-lg lg:shrink-0">
+            Layer 2 Ecosystem
+          </h2>
+          <Card>Soon™</Card>
+        </section>
+
+        <div className="mt-20">
+          <ValidatorCount />
+          <DeFiTotalValueLocked />
+          <Layer2Data />
+          <UltrasoundMoney />
+          <RealWorldAssets chartData={stablecoinChartData} />
+        </div>
       </article>
     </main>
   )
 }
 
 export const metadata: Metadata = {
-  title: "PAGE TITLE",
-  description: "PAGE DESCRIPTION",
+  title: "Data Hub: Real-Time Intelligence",
+  description:
+    "Track live, real-time data for mainnet activity, Layer-2 scaling, tokenized assets, and DeFi markets",
 }
