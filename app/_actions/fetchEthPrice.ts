4240247fb9cbcbaa9aafb8f76d63aa356fe9ab85
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-10-14 09:21:33 -0700

    feat: add fetchEthPrice

---
+++ app/_actions/fetchEthPrice.ts
@@ -0,0 +1,45 @@
+"use server"
+
+import type { DataTimestamped } from "@/lib/types"
+
+type JSONData = { ethereum: { usd: number } }
+
+export type EthPriceData = {
+  usd: number
+}
+
+export const fetchEthPrice = async (): Promise<
+  DataTimestamped<EthPriceData>
+> => {
+  const url =
+    "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+
+  try {
+    const response = await fetch(url, {
+      next: {
+        revalidate: 60 * 60, // 1 hour
+        tags: ["coingecko:v3:simple:price"],
+      },
+    })
+
+    if (!response.ok)
+      throw new Error(
+        `Fetch response not OK from ${url}: ${response.status} ${response.statusText}`
+      )
+
+    const json: JSONData = await response.json()
+
+    const { usd } = json.ethereum
+
+    return { data: { usd }, lastUpdated: Date.now() }
+  } catch (error: unknown) {
+    console.error("fetchEthPrice failed", {
+      name: error instanceof Error ? error.name : undefined,
+      message: error instanceof Error ? error.message : String(error),
+      url,
+    })
+    throw error
+  }
+}
+
+export default fetchEthPrice
