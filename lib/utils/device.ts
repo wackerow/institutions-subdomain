412e0ff660669e09cc1438d584fe13a301c2ee9e
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-10-15 12:04:19 -0700

    feat: add device size / use breakpoint utils

--- lib/utils/device.ts
+++ lib/utils/device.ts
@@ -4,3 +4,31 @@ export const isMobile = (): boolean => {
     window.navigator.userAgent
   )
 }
+
+/**
+ * Tailwind CSS defaults
+ * https://tailwindcss.com/docs/responsive-design#using-custom-breakpoints
+ *
+ * Variant	Media query
+ * max-sm	@media (width < 40rem)
+ * max-md	@media (width < 48rem)
+ * max-lg	@media (width < 64rem)
+ * max-xl	@media (width < 80rem)
+ * max-2xl	@media (width < 96rem)
+ */
+export const screens = {
+  xm: "480px",
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1536px",
+} as const satisfies Record<string, `${number}px`>
+
+export const breakpointAsNumber = Object.entries(screens).reduce(
+  (acc: Record<string, number>, [breakpoint, value]) => {
+    acc[breakpoint] = +value.split("px")[0]
+    return acc
+  },
+  {}
+) as Record<keyof typeof screens, number>
