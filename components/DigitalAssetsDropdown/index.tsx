d8252e69d3f12abc6ce37f61025904c325574886
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-10-15 12:04:56 -0700

    patch: update dropdown link reference

--- components/DigitalAssetsDropdown/index.tsx
+++ components/DigitalAssetsDropdown/index.tsx
@@ -5,32 +5,15 @@ import { Triangle } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
+import { DA_NAV_ITEM_LINKS } from "@/lib/constants"
+
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu"
-import Link, { LinkProps } from "../ui/link"
-
-export const digitalAssetsLinks: LinkProps[] = [
-  {
-    children: "Real-World Assets (RWAs)",
-    href: "/rwa",
-  },
-  {
-    children: "Decentralized Finance (DeFi)",
-    href: "/defi",
-  },
-  {
-    children: "Compliant Privacy",
-    href: "/privacy",
-  },
-  {
-    children: "Layer 2 (L2) Ecosystem",
-    href: "/layer-2",
-  },
-]
+import Link from "../ui/link"
 
 const DigitalAssetsDropdown = ({ className }: { className?: string }) => {
   const [open, setOpen] = useState(false)
@@ -47,7 +30,7 @@ const DigitalAssetsDropdown = ({ className }: { className?: string }) => {
         <Triangle className="inline size-[0.75em] -scale-y-75 fill-current transition-transform" />
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        {digitalAssetsLinks.map((props) => (
+        {DA_NAV_ITEM_LINKS.map((props) => (
           <DropdownMenuItem key={props.href}>
             <Link
               onClick={() => setOpen(false)}
